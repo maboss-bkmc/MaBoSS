cb2e98dd84aab9a12e806c2e68f6c08ea5b24a26
vnoel <vincent.noel@curie.fr>
2019-11-20 15:01:09 +0100

    First test for linux & mac

--- engine/tests/test-cellcycle.sh
+++ engine/tests/test-cellcycle.sh
@@ -5,6 +5,8 @@
 
 . `dirname $0`/share.sh
 
+return_code=0
+
 if [ ! -x $MABOSS ]
 then
     echo $MABOSS not found
@@ -13,37 +15,43 @@ fi
 
 check_file()
 {
-    echo
+    # echo
     if [ $? = 0 ]; then
 	echo "File $1 OK"
     else
 	echo "File $1 ** error: differences found **"
+    return_code=1
     fi
 }
 
 echo
 echo "Non regression test: Cell Cycle one thread"
+rm -rf tmp; mkdir -p tmp
 $LAUNCHER /usr/bin/time -p $MABOSS cellcycle.bnd -c cellcycle_runcfg.cfg -c cellcycle_runcfg-thread_1.cfg -o tmp/Cell_cycle_thread_1
 if [ $? != 0 ]; then exit 1; fi
-diff refer/Cell_cycle_thread_1_probtraj.csv tmp/
+python compare_probtrajs.py refer/Cell_cycle_thread_1_probtraj.csv tmp/Cell_cycle_thread_1_probtraj.csv --exact
 check_file "projtraj"
 
-diff refer/Cell_cycle_thread_1_statdist.csv tmp/ # || echo '**** error test #1.b (non regression) ****'
-check_file "statdist"
+# diff refer/Cell_cycle_thread_1_statdist.csv tmp/ # || echo '**** error test #1.b (non regression) ****'
+# check_file "statdist"
 
 if [ "$ONE_THREAD_ONLY" != "" ]; then exit 0; fi
 
-echo
-echo "Non regression test: Cell Cycle 6 threads"
-$LAUNCHER $MABOSS cellcycle.bnd -c cellcycle_runcfg.cfg -c cellcycle_runcfg-thread_6.cfg -o tmp/Cell_cycle_thread_6
+# echo
+# echo "Non regression test: Cell Cycle 6 threads"
+# $LAUNCHER /usr/bin/time -p $MABOSS cellcycle.bnd -c cellcycle_runcfg.cfg -c cellcycle_runcfg-thread_6.cfg -o tmp/Cell_cycle_thread_6
 
-diff refer/Cell_cycle_thread_6_probtraj.csv tmp/ # || echo '**** error test #2.a (non regression) ****'
-check_file "projtraj"
+# python compare_probtrajs.py refer/Cell_cycle_thread_6_probtraj.csv tmp/Cell_cycle_thread_6_probtraj.csv
+# check_file "projtraj"
 
-diff refer/Cell_cycle_thread_6_statdist.csv tmp/ # || echo '**** error test #2.b (non regression) ****'
-check_file "statdist"
+# diff refer/Cell_cycle_thread_6_statdist.csv tmp/ # || echo '**** error test #2.b (non regression) ****'
+# check_file "statdist"
 
-echo
-echo "Non regression test: checking differences between one and 6 threads results"
-diff tmp/Cell_cycle_thread_1_statdist.csv tmp/Cell_cycle_thread_6_statdist.csv # || echo '**** error test #3 (multi threads) ****'
-check_file "statdist"
+# echo
+# echo "Non regression test: checking differences between one and 6 threads results"
+# python compare_probtrajs.py tmp/Cell_cycle_thread_1_probtraj.csv tmp/Cell_cycle_thread_6_probtraj.csv --exact # || echo '**** error test #3 (multi threads) ****'
+# check_file "probtrajs"
+# python compare_probtrajs.py tmp/Cell_cycle_thread_1_statdist.csv tmp/Cell_cycle_thread_6_statdist.csv # || echo '**** error test #3 (multi threads) ****'
+# check_file "statdist"
+
+exit $return_code
