850701613e8f74d0ab7e41bb8c940f3484de3088
Vincent Noël <contact@vincent-noel.fr>
2019-12-17 13:15:53 +0100

    Added script to build manylinux packages

---
+++ engine/python/build-wheels.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e -x
+
+# Install a system package required by our library
+# yum install -y atlas-devel
+
+# Compile wheels for python 3.*
+for PYBIN in /opt/python/cp3*/bin; do
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/cp3*/bin/; do
+    "${PYBIN}/pip" install maboss_module --no-index -f /io/wheelhouse
+    # (cd "$HOME"; "${PYBIN}/nosetests" maboss_module)
+done
