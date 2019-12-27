2dec9fbc9a0a0a11f39abb7a3d97df8f3863df95
Vincent Noël <contact@vincent-noel.fr>
2019-12-27 19:32:26 +0100

    Added a test for the client/server

--- engine/tests/share.sh
+++ engine/tests/share.sh
@@ -1,4 +1,5 @@
 
 if [ -z "$MABOSS" ] ; then MABOSS=../src/MaBoSS; fi
 if [ -z "$MABOSS_CLIENT" ] ; then MABOSS_CLIENT=../src/MaBoSS-client; fi
+if [ -z "$MABOSS_SERVER" ] ; then MABOSS_SERVER=../src/MaBoSS-server; fi
 export DYLD_LIBRARY_PATH=../src:$DYLD_LIBRARY_PATH
---
+++ engine/tests/test-server.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+# test1.sh
+#
+
+. `dirname $0`/share.sh
+
+return_code=0
+
+if [ ! -x $MABOSS ]
+then
+    echo $MABOSS not found
+    exit 1
+fi
+
+check_file()
+{
+    if [ $? = 0 ]; then
+	echo "File $1 OK"
+    else
+	echo "File $1 ** error: differences found **"
+    return_code=1
+    fi
+}
+
+echo
+echo "Server test"
+rm -rf tmp; mkdir -p tmp
+$MABOSS_SERVER --host 0.0.0.0 --port 7777 &
+sleep 5s
+$LAUNCHER /usr/bin/time -p $MABOSS_CLIENT --host localhost --port 7777 cellcycle/cellcycle.bnd -c cellcycle/cellcycle_runcfg.cfg -c cellcycle/cellcycle_runcfg-thread_1.cfg -o tmp/Cell_cycle_thread_1
+if [ $? != 0 ]; then exit 1; fi
+python compare_probtrajs.py cellcycle/refer/Cell_cycle_thread_1_probtraj.csv tmp/Cell_cycle_thread_1_probtraj.csv --exact
+check_file "projtraj"
+
+python compare_statdist.py cellcycle/refer/Cell_cycle_thread_1_statdist.csv tmp/Cell_cycle_thread_1_statdist.csv --exact # || echo '**** error test #1.b (non regression) ****'
+check_file "statdist"
+
+rm -rf tmp; 
+
+exit $return_code
