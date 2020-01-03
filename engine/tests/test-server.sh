02f55856ae75ef941ce843017ed1c2e574b788ae
Vincent Noël <contact@vincent-noel.fr>
2020-01-03 18:49:51 +0100

    Server now exiting cleanly

--- engine/tests/test-server.sh
+++ engine/tests/test-server.sh
@@ -27,7 +27,8 @@ echo
 echo "Server test"
 rm -rf tmp; mkdir -p tmp
 $MABOSS_SERVER -q --host 0.0.0.0 --port 7777 &
-$MABOSS_SERVER_128n -q --host 0.0.0.0 --port 7778 &
+SERVER_PID=$!;
+echo "Launched server on pid ${SERVER_PID}";
 sleep 5s
 
 
@@ -50,6 +51,12 @@ check_file "projtraj"
 python compare_statdist.py cellcycle/refer/Cell_cycle_thread_6_statdist.csv tmp/Cell_cycle_thread_6_statdist.csv --exact # || echo '**** error test #1.b (non regression) ****'
 check_file "statdist"
 
+kill $SERVER_PID;
+$MABOSS_SERVER_128n -q --host 0.0.0.0 --port 7778 &
+SERVER_128_PID=$!;
+echo "Launched server on pid ${SERVER_128_PID}";
+sleep 5s
+
 
 $LAUNCHER /usr/bin/time -p $MABOSS_CLIENT --host 127.0.0.1 --port 7778 ewing/ewing_full.bnd -c ewing/ewing.cfg -c ewing/ewing_runcfg-thread_1.cfg -o tmp/ewing_thread_1
 if [ $? != 0 ]; then exit 1; fi
@@ -69,7 +76,7 @@ check_file "projtraj"
 python compare_statdist.py ewing/refer/ewing_thread_6_statdist.csv tmp/ewing_thread_6_statdist.csv --exact #|| echo '**** error test #2.b (non regression) ****'
 check_file "statdist"
 
-
+kill $SERVER_128_PID;
 rm -rf tmp; 
 
 exit $return_code
