c6c48327c37275e2794a3dd5e4b4c5c92743daa4
Vincent Noël <contact@vincent-noel.fr>
2020-01-06 16:36:03 +0100

    Added prngs tests

---
+++ engine/tests/test-prngs.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#
+# test1.sh
+#
+
+. `dirname $0`/share.sh
+
+return_code=0
+
+if [ ! -x $MABOSS ]
+then
+    echo $MABOSS not found
+    exit 1
+fi
+
+check_file()
+{
+    # echo
+    if [ $? = 0 ]; then
+	echo "File $1 OK"
+    else
+	echo "File $1 ** error: differences found **"
+    return_code=1
+    fi
+}
+
+echo
+echo "Non regression test: Cell Cycle one thread"
+rm -rf tmp; mkdir -p tmp
+$LAUNCHER /usr/bin/time -p $MABOSS prngs/cellcycle.bnd -c prngs/cellcycle_runcfg.cfg -c prngs/glibc.cfg -o tmp/Cell_cycle_glibc
+if [ $? != 0 ]; then exit 1; fi
+python compare_probtrajs.py prngs/refer/Cell_cycle_glibc_probtraj.csv tmp/Cell_cycle_glibc_probtraj.csv --exact
+check_file "projtraj"
+
+python compare_statdist.py prngs/refer/Cell_cycle_glibc_statdist.csv tmp/Cell_cycle_glibc_statdist.csv --exact # || echo '**** error test #1.b (non regression) ****'
+check_file "statdist"
+
+$LAUNCHER /usr/bin/time -p $MABOSS prngs/cellcycle.bnd -c prngs/cellcycle_runcfg.cfg -c prngs/mt.cfg -o tmp/Cell_cycle_mt
+if [ $? != 0 ]; then exit 1; fi
+python compare_probtrajs.py prngs/refer/Cell_cycle_mt_probtraj.csv tmp/Cell_cycle_mt_probtraj.csv --exact
+check_file "projtraj"
+
+python compare_statdist.py prngs/refer/Cell_cycle_mt_statdist.csv tmp/Cell_cycle_mt_statdist.csv --exact # || echo '**** error test #1.b (non regression) ****'
+check_file "statdist"
+
+$LAUNCHER /usr/bin/time -p $MABOSS prngs/cellcycle.bnd -c prngs/cellcycle_runcfg.cfg -c prngs/physical.cfg -o tmp/Cell_cycle_phys
+if [ $? != 0 ]; then exit 1; fi
+
+# echo "Comparing aproximatively the results of the different prngs"
+# python compare_probtrajs.py tmp/Cell_cycle_glibc_probtraj.csv tmp/Cell_cycle_mt_probtraj.csv # || echo '**** error test #3 (multi threads) ****'
+# check_file "probtrajs"
+# python compare_probtrajs.py tmp/Cell_cycle_glibc_probtraj.csv tmp/Cell_cycle_phys_probtraj.csv # || echo '**** error test #3 (multi threads) ****'
+# check_file "probtrajs"
+
+exit $return_code
