a97d1527ad86acf78207685fb868d5c19d4590a2
Vincent Noël <contact@vincent-noel.fr>
2020-01-07 19:07:24 +0100

    Tagging alpha5

--- engine/python/setup.py
+++ engine/python/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 from sys import executable, argv
 from os.path import join, dirname
 
-maboss_version = '1.0.0-alpha-4'
+maboss_version = '1.0.0-alpha-5'
 
 maboss_sources = [
    "MetaEngine.cc", "MaBEstEngine.cc", "EnsembleEngine.cc", "Cumulator.cc", "ProbaDist.cc", 
