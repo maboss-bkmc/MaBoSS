e448d274124c5ff88a161415498227de7fd24e93
Vincent Noël <contact@vincent-noel.fr>
2020-01-16 20:12:27 +0100

    Added node test for final state simulation

--- engine/python/test.py
+++ engine/python/test.py
@@ -24,16 +24,28 @@ class TestCMaBoSS(TestCase):
             'Migration -- Metastasis -- Invasion -- CellCycleArrest': 0.3
         }
 
+        expected_nodes = {
+            'Metastasis': 0.3, 'Invasion': 0.3, 'Migration': 0.3, 'Apoptosis': 0.426, 'CellCycleArrest': 0.794
+        }
+
         sim = cmaboss.MaBoSSSim(network="../examples/metastasis.bnd", config="../examples/metastasis.cfg")
         res = sim.run(only_last_state=False)
+
         for key, value in res.get_last_states_probtraj().items():
             self.assertAlmostEqual(value, expected[key])
         
+        for key, value in res.get_last_nodes_probtraj().items():
+            self.assertAlmostEqual(value, expected_nodes[key])
+        
         simfinal = cmaboss.MaBoSSSim(network="../examples/metastasis.bnd", config="../examples/metastasis.cfg")
-        resfinal = simfinal.run(only_last_state=True)    
+        resfinal = simfinal.run(only_last_state=True)
+    
         for key, value in resfinal.get_last_states_probtraj().items():
             self.assertAlmostEqual(value, expected[key])
-            
+
+        for key, value in resfinal.get_last_nodes_probtraj().items():
+            self.assertAlmostEqual(value, expected_nodes[key])
+           
     def test_load_model_str(self):
         with open("../examples/metastasis.bnd", "r") as bnd, open("../examples/metastasis.cfg", "r") as cfg:    
             sim = cmaboss.MaBoSSSim(network_str=bnd.read(),config_str=cfg.read())
--- engine/python/test_128n.py
+++ engine/python/test_128n.py
@@ -24,16 +24,26 @@ class TestCMaBoSS(TestCase):
             'Migration -- Metastasis -- Invasion -- CellCycleArrest': 0.3
         }
 
+        expected_nodes = {
+            'Metastasis': 0.3, 'Invasion': 0.3, 'Migration': 0.3, 'Apoptosis': 0.426, 'CellCycleArrest': 0.794
+        }
+
         sim = cmaboss_128n.MaBoSSSim(network="../examples/metastasis.bnd", config="../examples/metastasis.cfg")
         res = sim.run(only_last_state=False)
         for key, value in res.get_last_states_probtraj().items():
             self.assertAlmostEqual(value, expected[key])
+        
+        for key, value in res.get_last_nodes_probtraj().items():
+            self.assertAlmostEqual(value, expected_nodes[key])
         
         simfinal = cmaboss_128n.MaBoSSSim(network="../examples/metastasis.bnd", config="../examples/metastasis.cfg")
         resfinal = simfinal.run(only_last_state=True)    
         for key, value in resfinal.get_last_states_probtraj().items():
             self.assertAlmostEqual(value, expected[key])
 
+        for key, value in resfinal.get_last_nodes_probtraj().items():
+            self.assertAlmostEqual(value, expected_nodes[key])
+        
     def test_load_model_str(self):
         with open("../tests/ewing/ewing_full.bnd", "r") as bnd, open("../tests/ewing/ewing.cfg", "r") as cfg:    
             sim = cmaboss_128n.MaBoSSSim(network_str=bnd.read(),config_str=cfg.read())
