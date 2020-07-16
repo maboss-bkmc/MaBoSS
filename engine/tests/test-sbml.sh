c8e87eff58b9999486efe57f059bbfe9dbdd60e4
Vincent Noël <contact@vincent-noel.fr>
2020-07-16 17:05:17 +0200

    Added basic sbml test

---
+++ engine/tests/test-sbml.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+# test-sbml.sh
+# (very) basic test of SBML qual compatibility
+
+. `dirname $0`/share.sh
+
+return_code=0
+
+if [ ! -x $MABOSS ]
+then
+    echo $MABOSS not found
+    exit 1
+fi
+
+check_file()
+{
+    # echo
+    if [ $? = 0 ]; then
+	echo "File $1 OK"
+    else
+	echo "File $1 ** error: differences found **"
+    return_code=1
+    fi
+}
+
+echo
+echo "Testing SBML compatibility"
+rm -rf tmp; mkdir -p tmp
+$LAUNCHER /usr/bin/time -p $MABOSS sbml/cell_fate.sbml -c sbml/cell_fate.cfg -o tmp/cell_fate
+if [ $? != 0 ]; then exit 1; fi
+python compare_probtrajs.py sbml/refer/cell_fate_probtraj.csv tmp/cell_fate_probtraj.csv --exact
+check_file "projtraj"
+
+exit $return_code
