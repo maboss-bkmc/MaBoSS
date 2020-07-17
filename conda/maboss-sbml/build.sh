325cc530bfb7aa007f1cdf03b64a77e4e0baeee1
Vincent Noël <contact@vincent-noel.fr>
2020-07-17 19:01:29 +0200

    Restored all maboss versions

--- conda/maboss-sbml/build.sh
+++ conda/maboss-sbml/build.sh
@@ -4,10 +4,10 @@ export EXTRA_CXXFLAGS=-I${PREFIX}/include
 export EXTRA_LDFLAGS="-L${PREFIX}/lib -lxml2"
 
 make SBML_COMPAT=1 install
-# make SBML_COMPAT=1 MAXNODES=128 install
-# make SBML_COMPAT=1 MAXNODES=256 install
-# make SBML_COMPAT=1 MAXNODES=512 install
-# make SBML_COMPAT=1 MAXNODES=1024 install
+make SBML_COMPAT=1 MAXNODES=128 install
+make SBML_COMPAT=1 MAXNODES=256 install
+make SBML_COMPAT=1 MAXNODES=512 install
+make SBML_COMPAT=1 MAXNODES=1024 install
 mkdir -p ${PREFIX}/bin
 mv ../pub/MaBoSS  ../pub/MaBoSS_*n ${PREFIX}/bin
 mv ../pub/MaBoSS-server  ../pub/MaBoSS_*n-server ${PREFIX}/bin
