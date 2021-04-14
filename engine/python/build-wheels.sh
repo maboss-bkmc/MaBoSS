efafadd09efd0d067ebcb57739683f6e7d9f841f
Vincent Noël <contact@vincent-noel.fr>
2021-04-14 12:12:02 +0200

    Added libsbml install in build-wheels.sh

--- engine/python/build-wheels.sh
+++ engine/python/build-wheels.sh
@@ -3,7 +3,31 @@ set -e -x
 
 # Install a system package required by our library
 # yum install -y atlas-devel
-
+yum install -y wget libxml2-devel
+wget https://sourceforge.net/projects/sbml/files/libsbml/5.19.0/stable/libSBML-5.19.0-core-plus-packages-src.tar.gz
+tar -zxf libSBML-5.19.0-core-plus-packages-src.tar.gz
+cd libSBML-5.19.0-Source
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr \
+-DCMAKE_INSTALL_LIBDIR=/usr/lib \
+-DCMAKE_CXX_COMPILER=g++ \
+-DCMAKE_C_COMPILER=gcc \
+-DCMAKE_CXX_STANDARD_LIBRARIES=-lxml2 \
+-DWITH_SWIG=OFF \
+-DLIBXML_LIBRARY=/usr/lib \
+-DLIBXML_INCLUDE_DIR=/usr/include/libxml2 \
+-DENABLE_COMP=ON \
+-DENABLE_FBC=ON \
+-DENABLE_GROUPS=ON \
+-DENABLE_LAYOUT=ON \
+-DENABLE_MULTI=ON \
+-DENABLE_QUAL=ON \
+-DENABLE_RENDER=ON \
+-DENABLE_DISTRIB=ON \
+..
+make
+sudo make install
 # Compile wheels for python 3.*
 for PYBIN in /opt/python/cp3*/bin; do
     "${PYBIN}/pip" install numpy
