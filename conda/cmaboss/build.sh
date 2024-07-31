b9940ef640db6d4453b66ee9f1c7cce3a508763e
Vincent Noël <vincent.noel@curie.fr>
2024-07-31 16:01:50 -0300

    Update conda cmaboss package for numpy 2.0

--- conda/cmaboss/build.sh
+++ conda/cmaboss/build.sh
@@ -2,7 +2,7 @@ cd engine/src
 make grammars
 cd ../python
 cp -r ../src cmaboss
-$PYTHON -m pip install .
+$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 mkdir -p ${PREFIX}/tests
 cp -r ../tests/* ${PREFIX}/tests
 
