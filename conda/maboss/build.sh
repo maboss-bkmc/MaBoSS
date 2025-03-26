7104e2babaa621d27157d0a60a3e6ccb485701ea
Vincent Noël <vincent.noel@curie.fr>
2025-03-26 16:02:30 -0300

    Using Makefile's CLIENT_SERVER option in containers and conda package

--- conda/maboss/build.sh
+++ conda/maboss/build.sh
@@ -3,11 +3,11 @@ cd engine/src
 # export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -I${PREFIX}/include/libxml2"
 export LDFLAGS="${LDFLAGS} -L${BUILD_PREFIX}/lib -lxml2"
 
-make SBML_COMPAT=1 install
-make SBML_COMPAT=1 MAXNODES=128 install
-make SBML_COMPAT=1 MAXNODES=256 install
-make SBML_COMPAT=1 MAXNODES=512 install
-make SBML_COMPAT=1 MAXNODES=1024 install
+make SBML_COMPAT=1 CLIENT_SERVER=1 install
+make SBML_COMPAT=1 CLIENT_SERVER=1 MAXNODES=128 install
+make SBML_COMPAT=1 CLIENT_SERVER=1 MAXNODES=256 install
+make SBML_COMPAT=1 CLIENT_SERVER=1 MAXNODES=512 install
+make SBML_COMPAT=1 CLIENT_SERVER=1 MAXNODES=1024 install
 mkdir -p ${PREFIX}/bin
 mv ../pub/MaBoSS  ../pub/MaBoSS_*n ${PREFIX}/bin
 mv ../pub/PopMaBoSS  ../pub/PopMaBoSS_*n ${PREFIX}/bin
