c1a854b584085b353641d0502caca446d1b2e42b
Vincent Noël <vincent.noel@curie.fr>
2025-03-27 08:44:47 -0300

    Allowing to set a BIN path for tests

--- engine/tests/share.sh
+++ engine/tests/share.sh
@@ -1,15 +1,21 @@
 
-if [ -z "$MABOSS" ] ; then MABOSS=../src/MaBoSS; fi
-if [ -z "$MABOSS_MPI" ] ; then MABOSS_MPI=../src/MaBoSS.MPI; fi
-if [ -z "$MABOSS_DN" ] ; then MABOSS_DN=../src/MaBoSS_dn; fi
-#if [ -z "$MABOSS_BITSET" ] ; then MABOSS_BITSET=../src/MaBoSS_bitset; fi
-if [ -z "$MABOSS_128n" ] ; then MABOSS_128n=../src/MaBoSS_128n; fi
-if [ -z "$MABOSS_128n_MPI" ] ; then MABOSS_128n_MPI=../src/MaBoSS_128n.MPI; fi
-if [ -z "$MABOSS_CLIENT" ] ; then MABOSS_CLIENT=../src/MaBoSS-client; fi
-if [ -z "$MABOSS_SERVER" ] ; then MABOSS_SERVER=../src/MaBoSS-server; fi
-if [ -z "$MABOSS_SERVER_128n" ] ; then MABOSS_SERVER_128n=../src/MaBoSS_128n-server; fi
-if [ -z "$POPMABOSS" ] ; then POPMABOSS=../src/PopMaBoSS; fi
-if [ -z "$POPMABOSS_MPI" ] ; then POPMABOSS_MPI=../src/PopMaBoSS.MPI; fi
+
+
+if [ -z "$MABOSS_BIN_PATH" ] ; then 
+    MABOSS_BIN_PATH=../pub; 
+fi
+
+if [ -z "$MABOSS" ] ; then MABOSS=$MABOSS_BIN_PATH/MaBoSS; fi
+if [ -z "$MABOSS_MPI" ] ; then MABOSS_MPI=$MABOSS_BIN_PATH/MaBoSS.MPI; fi
+if [ -z "$MABOSS_DN" ] ; then MABOSS_DN=$MABOSS_BIN_PATH/MaBoSS_dn; fi
+if [ -z "$MABOSS_128n" ] ; then MABOSS_128n=$MABOSS_BIN_PATH/MaBoSS_128n; fi
+if [ -z "$MABOSS_128n_MPI" ] ; then MABOSS_128n_MPI=$MABOSS_BIN_PATH/MaBoSS_128n.MPI; fi
+if [ -z "$MABOSS_CLIENT" ] ; then MABOSS_CLIENT=$MABOSS_BIN_PATH/MaBoSS-client; fi
+if [ -z "$MABOSS_SERVER" ] ; then MABOSS_SERVER=$MABOSS_BIN_PATH/MaBoSS-server; fi
+if [ -z "$MABOSS_SERVER_128n" ] ; then MABOSS_SERVER_128n=$MABOSS_BIN_PATH/MaBoSS_128n-server; fi
+if [ -z "$POPMABOSS" ] ; then POPMABOSS=$MABOSS_BIN_PATH/PopMaBoSS; fi
+if [ -z "$POPMABOSS_MPI" ] ; then POPMABOSS_MPI=$MABOSS_BIN_PATH/PopMaBoSS.MPI; fi
+
 export DYLD_LIBRARY_PATH=../src:$DYLD_LIBRARY_PATH
 
 if [[ ! -z "$USE_MABOSS_DYNBITSET" ]]; then
