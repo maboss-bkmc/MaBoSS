722c31b9ad4af6ef81da8915bbfe5ac8cd0b7621
Vincent Noël <vincent.noel@curie.fr>
2025-04-14 08:21:52 -0300

    Fix userfunc test

--- scripts/run_userfunc_tests.sh
+++ scripts/run_userfunc_tests.sh
@@ -19,6 +19,20 @@ if [[ $1 == "cmake" ]]; then
         -DUSERFUNC=./engine/tests/user_func/user_func.cc
     cmake --build build
     cmake --install build
+    cd engine/tests/
+    if [[ $2 == "win" ]]; then
+        bash ./test-user_func.sh ../bin/user_func.dll
+        check "user_func" 
+    else
+        if [[ $2 == "mac" ]]; then
+            bash ./test-user_func.sh ../lib/libuser_func.dylib
+            check "user_func"    
+        else
+            bash ./test-user_func.sh ../lib/libuser_func.so
+            check "user_func"
+        fi
+    fi
+    
 else 
     cd engine
     cp tests/user_func/user_func.cc src
@@ -28,14 +42,13 @@ else
     if [ $? != 0 ]; then exit 1; fi
     mkdir -p ../lib
     cp libuser_func.so ../lib
-    cd ../..
+    cd ../tests/
+    bash ./test-user_func.sh ../lib/libuser_func.so
+    check "user_func"
     
 fi
 
 
-cd engine/tests/
-bash ./test-user_func.sh ../lib/libuser_func.so
-check "user_func"
 
 cd ../..
 
